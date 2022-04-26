8e1a311e35e3baada528cf6c883d3d7d13cfa090
Sergio Chica <sergio.chica@uc3m.es>
2022-04-26 15:43:38 +0200

    Created arp monitor and scanner. On arp discovered, cvescannerv2 is launched

---
+++ netauditor.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# utils - Utilities module.
+
+# Copyright (C) 2022 Sergio Chica Manjarrez @ pervasive.it.uc3m.es.
+# Universidad Carlos III de Madrid.
+
+# This file is part of autoauditor.
+
+# autoauditor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# autoauditor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+# from zerconf import ServiceBrowser, ZeroConf  # apple devices
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import scapy.all as scp
+import utils as ut
+import threading
+import ipaddress
+import netifaces
+import datetime
+import argparse
+
+
+class ARPMonitor(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.cache = {}
+        self.daemon = True
+
+    def run(self):
+        scp.sniff(prn=self.arp_monitor, filter="arp", store=0)
+
+    def arp_monitor(self, packet):
+        if scp.ARP in packet:
+            arp = ut.ARPPacket(packet[scp.ARP])
+            target = None
+            if arp.hwsrc not in self.cache and not arp.hwdst_null():
+                self.cache[arp.hwsrc] = arp.psrc
+                ut.log('succ',
+                       f"New device detected ({arp.op}): "
+                       f"{arp.hwsrc} - {arp.psrc}")
+                target = (arp.hwsrc, arp.psrc)
+            if arp.op == 'is-at':
+                if arp.hwdst not in self.cache and not arp.hwdst_null():
+                    self.cache[arp.hwdst] = arp.pdst
+                    ut.log('succ',
+                           f"New device detected ({arp.op}): "
+                           f"{arp.hwdst} - {arp.pdst}")
+                    target = (arp.hwdst, arp.pdst)
+                elif (arp.hwdst in self.cache and
+                      arp.pdst != self.cache[arp.hwdst]):
+                    ut.log('warn',
+                           f"Inconsistency detected ({arp.op}): "
+                           f"{arp.hwdst} - {self.cache[arp.hwdst]} -> "
+                           f"{arp.pdst}")
+                    target = (arp.hwdst, arp.pdst)
+            if target is not None:
+                ut.log('info',
+                       f"Starting analysis: {target[0]} - {target[1]}")
+                th = ut.NmapScanner(*target)
+                th.start()
+
+
+def arp_scanner():
+    ut.log('info', "Running scheduled job...")
+    subnets = []
+    for name in netifaces.interfaces():
+        if (name != 'lo' and
+            not name.startswith('docker') and
+            not name.startswith('vmnet')):  # noqa
+            if netifaces.AF_INET in netifaces.ifaddresses(name):
+                iface = netifaces.ifaddresses(name)[netifaces.AF_INET]
+                for sn in iface:
+                    ip = ipaddress.ip_interface(
+                        f'{sn["addr"]}/{sn["netmask"]}')
+                    subnet = str(ip.network)
+                    if subnet not in subnets:
+                        subnets.append(subnet)
+                        ans, unans = scp.srp(
+                            scp.Ether(dst="ff:ff:ff:ff:ff:ff") /
+                            scp.ARP(pdst=subnet), timeout=2, verbose=0)
+                        # ans.summary(
+                        #     lambda src, recv:
+                        #     recv.sprintf("%Ether.src% %ARP.psrc%"))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='python netuaditor.py',
+        description="Network scanner and analyzer.")
+    parser.add_argument('--verbose', action='store_true',
+                        help="Verbose output.")
+    args = parser.parse_args()
+    if args.verbose:
+        ut.log_verbose()
+
+    ut.create_dirs()
+
+    monitor = ARPMonitor()
+    monitor.start()
+    scheduler = BackgroundScheduler(timezone="Europe/Madrid")
+    scheduler.add_job(arp_scanner, 'interval', minutes=5,
+                      next_run_time=datetime.datetime.now())
+    scheduler.start()
+
+    import time
+    time.sleep(240)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,121 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# utils - Utilities module.
+
+# Copyright (C) 2022 Sergio Chica Manjarrez @ pervasive.it.uc3m.es.
+# Universidad Carlos III de Madrid.
+
+# This file is part of autoauditor.
+
+# autoauditor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# autoauditor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+import threading
+import nmap3
+import re
+
+CVE_LOG = 'output/log'
+CVE_RAW = 'output/raw'
+CVE_JSON = 'output/json'
+
+COLORS = {
+    'R': '\033[91m',
+    'Y': '\033[93m',
+    'B': '\033[94m',
+    'G': '\033[92m',
+    'N': '\033[0m',
+    'E': ''
+}
+LOG = {
+    'normal': '',
+    'succ': '[+] ', 'info': '[*] ',
+    'warn': '[-] ', 'error': '[!] ',
+    'enabled': False}
+
+
+def disable_ansi_colors():
+    COLORS['R'] = COLORS['E']
+    COLORS['Y'] = COLORS['E']
+    COLORS['B'] = COLORS['E']
+    COLORS['G'] = COLORS['E']
+    COLORS['N'] = COLORS['E']
+
+
+def log_verbose():
+    LOG['enabled'] = True
+
+
+def log(ltype, msg, end='\n', err=None):
+    color = LOG[ltype]
+    if ltype == 'succ':
+        color = f'{COLORS["G"]}{color}{COLORS["N"]}'
+    elif ltype == 'info':
+        color = f'{COLORS["B"]}{color}{COLORS["N"]}'
+    elif ltype == 'warn':
+        color = f'{COLORS["Y"]}{color}{COLORS["N"]}'
+    elif ltype == 'error':
+        color = f'{COLORS["R"]}{color}{COLORS["N"]}'
+    if LOG['enabled']:
+        print(f"{color}{msg}", end=end, flush=True)
+
+
+def create_dirs():
+    try:
+        Path(CVE_LOG).mkdir(parents=True, exist_ok=True)
+        Path(CVE_RAW).mkdir(parents=True, exist_ok=True)
+        Path(CVE_JSON).mkdir(parents=True, exist_ok=True)
+    except PermissionError as ex:
+        log('error', f"{ex.strerror}: {ex.filename}.", err=ex.errno)
+
+
+class ARPPacket:
+    OP = {
+        1: 'who-has',
+        2: 'is-at'
+    }
+
+    def __init__(self, packet):
+        self.packet = packet
+
+    def __getattr__(self, name):
+        if name == 'op':
+            return self.OP[getattr(self.packet, name)]
+        else:
+            return getattr(self.packet, name)
+
+    def hwdst_null(self):
+        return self.packet.hwdst == '00:00:00:00:00:00'
+
+
+class NmapScanner(threading.Thread):
+    elem = re.compile(r'.*?: (.*)')
+    cve = re.compile(r'\s*([A-Z0-9-]*)\s*([0-9.]*)\s*([0-9.-]*)'
+                     r'\s*([A-Za-z]*)\s*([A-Za-z]*)')
+
+    def __init__(self, mac, target):
+        threading.Thread.__init__(self)
+        self.mac = mac
+        self.target = target
+        self.daemon = True
+
+    def run(self):
+        nm = nmap3.Nmap()
+        nm.run_command(['/usr/bin/nmap',
+                        '-oN', f'{CVE_RAW}/{self.mac}.raw',
+                        '-sV', self.target,
+                        '--script', 'cvescannerv2',
+                        '--script-args',
+                        f'log={CVE_LOG}/{self.mac}.log,'
+                        f'json={CVE_JSON}/{self.mac}.json'])
